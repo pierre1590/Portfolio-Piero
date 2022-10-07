0dec299af9ca60b59c49f3409d272dbf9afc72ce
Piero Sabino <piero.sa@icloud.com>
2022-10-07 22:45:22 +0200

    Update project

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,10 +10,10 @@ export const Contact = () => {
   return (
     <>
       <Container className="contact-container">
-      <h1>Get in touch</h1>
+        <h1>Get in touch</h1>
         <Row>
-        <Col md={6} className="contact-col">
-            <Form noValidate >
+          <Col md={6} className="contact-col">
+            <Form noValidate>
               <FloatingLabel
                 controlId="floatingInput"
                 label="Name"
@@ -39,7 +39,7 @@ export const Contact = () => {
                   Please provide a valid email.
                 </Form.Control.Feedback>
               </FloatingLabel>
-             
+
               <FloatingLabel
                 controlId="floatingInput"
                 label="Message"
@@ -56,33 +56,25 @@ export const Contact = () => {
                 </Form.Control.Feedback>
               </FloatingLabel>
               <Button variant="primary" type="submit" className="contact-btn">
-                Submit 
-                <MDBIcon icon='paper-plane' className="ms-2" />
+                Submit
+                <MDBIcon icon="paper-plane" className="ms-2" />
               </Button>
             </Form>
           </Col>
           <Col md={6} className="contact-col">
             <p className="contact-text">
-                If you want to chat, send me a message on Whatsapp
+              If you want to chat, send me a message on Whatsapp
             </p>
-            
-            
-            
             <a
-                href="https://wa.me/+393395997631"
-                target="_blank"
-                rel="noreferrer"
-                className="contact-link"
-                >
-               
-                {/* <IoLogoWhatsapp className="contact-icon" /> */}
-                +39 339 599 7631
-                
+              href="https://wa.me/+393395997631"
+              target="_blank"
+              rel="noreferrer"
+              className="contact-link"
+            >
+              +39 339 599 7631
             </a>
           </Col>
-          
         </Row>
-       
       </Container>
     </>
   );
