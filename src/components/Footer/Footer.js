4fb8fe2ccc33a9a456b9754fed0fb63b333ec8c1
Piero Sabino <piero.sa@icloud.com>
2022-10-07 11:19:25 +0200

    Update social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,21 +14,10 @@ export const Footer = () => {
         <MDBFooter className="bg-dark text-center text-white">
           <MDBContainer className="p-4 pb-0">
             <section className="mb-4" >
-              <a
-                className="btn btn-outline-light btn-floating m-1 instagram"
-                style={{ backgroundColor: "#ac2bac" }}
-                href="https://www.instagram.com/p137ru590/?hl=it"
-                role="button" 
-                rel="noreferrer"
-                target="_blank"
-               
-              >
-                <MDBIcon fab icon="instagram" />
-              </a>
 
-              <a
+            <a
                 className="btn btn-outline-light btn-floating m-1 linkedin"
-                style={{ backgroundColor: "#0082ca" }}
+              
                 href="https://www.linkedin.com/in/pierosabino/"
                 target="_blank"
                 rel="noreferrer"
@@ -37,9 +26,34 @@ export const Footer = () => {
                 <MDBIcon fab icon="linkedin-in" />
               </a>
 
+
+              <a
+                className="btn btn-outline-light btn-floating m-1 instagram"
+                
+                href="https://www.instagram.com/p137ru590/?hl=it"
+                role="button" 
+                rel="noreferrer"
+                target="_blank"
+               
+              >
+                <MDBIcon fab icon="instagram" />
+              </a>
+           
+
+              <a
+                className="btn btn-outline-light btn-floating m-1 twitter"
+               
+                href="https://twitter.com/SabinoPiero"
+                target="_blank"
+                rel="noreferrer"
+                role="button"
+              >
+                <MDBIcon fab icon="twitter" />
+              </a>
+
               <a
                 className="btn btn-outline-light btn-floating m-1 github"
-                style={{ backgroundColor: "#333333" }}
+              
                 href="https://github.com/pierre1590"
                 target="_blank"
                 rel="noreferrer"
